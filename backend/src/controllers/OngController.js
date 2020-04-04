9e8f31df5b9a99ca900205c76cfc9eed8ac5ea2b
Renan Lucas <renanluquinhas53@gmail.com>
2020-04-04 20:43:42 -0300

    complete application

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,27 @@
+import crypto from 'crypto';
+import connection from '../database/connection';
+
+export default {
+    async store(req, res) {
+        const { name, email, whatsapp, city, uf } = req.body;
+
+        const id = crypto.randomBytes(4).toString('HEX');
+    
+        await connection('ongs').insert({ 
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        })
+    
+        return res.json({ id });
+    },
+
+    async index(req, res) {
+        const ongs = await connection('ongs').select('*');
+
+        return res.json(ongs);
+    }
+}
